f9dd2316573b55a14eb3fc383a64dad12351d1e8
toro2k <toro2k@gmail.com>
2015-03-03 17:32:07 +0100

    Fix colored output in bundle.sh.

--- bundle.sh
+++ bundle.sh
@@ -4,7 +4,7 @@ BUNDLE_DIR="${PWD}/bundle"
 BUNDLE_LIST="${PWD}/bundle.list"
 
 if [ ! -r "$BUNDLE_LIST" ]; then
-    echo "${BUNDLE_LIST} is not readable" 2>&1
+    printf "${BUNDLE_LIST} is not readable\n" 2>&1
     exit 1
 fi
 
@@ -19,11 +19,11 @@ while read plugin_url; do
     plugin_name=$(basename "$plugin_url" ".git")
     plugin_path="${BUNDLE_DIR}/${plugin_name}"
 
-    echo -n "\033[01;34m${plugin_name}\033[0m: "
+    printf "\033[01;34m${plugin_name}\033[0m: "
 
     if [ ! -d "$plugin_path" ]; then
         git clone "$plugin_url" "$plugin_path"
     else
-        echo "already installed" # maybe update it
+        printf "already installed\n" # maybe update it
     fi
 done < "$BUNDLE_LIST"
